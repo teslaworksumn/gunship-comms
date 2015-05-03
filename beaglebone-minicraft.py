bb6ca772865034768d1aeb5c5a8c2379fb16b98b
Michael Diaz <diazx069@umn.edu>
2015-05-02 22:26:20 -0500

    Various changes On branch master Changes to be committed: 	new file:   beaglebone-minicraft.py 	modified:   driver_station.py

---
+++ beaglebone-minicraft.py
@@ -0,0 +1,49 @@
+"""
+Continuously read the serial port and process IO data received from a remote XBee.
+"""
+
+import sys
+import time
+import Adafruit_BBIO.PWM as PWM
+import EnttecUsbDmxProWidget
+
+dmx = EnttecUsbDmxProWidget.EnttecUsbDmxProWidget()
+dmx.setPort("/dev/ttyO2", 115200)
+dmx.connect()
+
+# Continuously read and print packets
+while True:
+    rx = dmx.getRecievedFrame()
+    ResponseArray = rx['frame']
+    print(ResponseArray)
+    try:
+        if ResponseArray != []:            
+            if ((ResponseArray[0] == 1) and ((ResponseArray[1] < .15) and (ResponseArray[1] > -.15))): #Brake
+                PWM.set_duty_cycle("P9_14", 25.5) #Input A CHANGE ONCE JACK/RYAN GET HERE
+                PWM.set_frequency("P9_14", 10) #Frequency A
+                PWM.set_duty_cycle("P9_15", 25.5) #Input B
+                PWM.set_frequency("P9_15", 10) #Frequency B
+                sleep(.02)
+                PWM.stop("P9_14") #stops for a second
+                PWM.stop("P9_15") #stops for a second
+
+            if ((ResponseArray[0] == 1) and (ResponseArray[1] >= .25)): #Reverse
+                #PWM.set_duty_cycle("P9_14", 25.5) Input A STAY COMMENTED OUT
+                #PWM.set_frequency("P9_14", 10) STAY COMMENTED OUT
+                PWM.set_duty_cycle("P9_15", 25.5) #Input B
+                PWM.set_frequency("P9_15", 10)
+                sleep(.02)
+                PWM.stop("P9_14") #stops for a second
+                PWM.stop("P9_15") #stops for a second
+
+            if ((ResponseArray[0] == 1) and (ResponseArray[1] <= -.25)): #Forward
+                PWM.set_duty_cycle("P9_14", 25.5) #Input A 
+                PWM.set_frequency("P9_14", 10) 
+                #PWM.set_duty_cycle("P9_15", 25.5) Input B. Stay commented out.
+                #PWM.set_frequency("P9_15", 10) Stay commented out.
+                sleep(.02)
+                PWM.stop("P9_14") #stops for a second
+                PWM.stop("P9_15") #stops for a second
+    except KeyboardInterrupt:
+        break
+    time.sleep(0.01)
--- driver_station.py
+++ driver_station.py
@@ -4,18 +4,10 @@ print '%s joystick(s) found.' % pygame.joystick.get_count()
 j = pygame.joystick.Joystick(0)
 j.init()
 print 'Initialized joystick: %s' % j.get_name()
-import serial
-from xbee import XBee,ZigBee
-import json
-jse = json.JSONEncoder()
-
-serial_port= serial.Serial()
-serial_port.port = "COM7"
-serial_port.baudrate = 9600
-if serial_port.isOpen():
-    disconnect()
-serial_port.open()
-print "Serial port opened on {0} at {1} baud".format(serial_port.port,serial_port.baudrate)
+import EnttecUsbDmxPro
+dmx = EnttecUsbDmxPro.EnttecUsbDmxPro()
+dmx.setPort("COM7", 115200)
+dmx.connect()
 
 def get():
     out = [0] * (j.get_numaxes() + j.get_numbuttons())
@@ -39,10 +31,10 @@ def test():
         curr_vals = get()
         for index, val in enumerate(curr_vals):
             if last_vals[index] != val:
-                d2s = [index, val]
-                
+                v2 = int((val+1)*127)
+                d2s = [index, v2]
                 print d2s
-                serial_port.write(bytearray(jse.encode(d2s)+'\r\l'))
+                dmx.sendDMX(d2s)
         last_vals = curr_vals
         
 
