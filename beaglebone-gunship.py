da2c5113a7b3110fee16ca08638d684c0050c1a0
Michael Diaz <diazx069@umn.edu>
2015-05-04 10:14:46 -0500

    Updated code to work with RS232 on Teensys On branch master Your branch is up-to-date with 'origin/master'. Changes to be committed: 	modified:   beaglebone-gunship.py 	renamed:    beaglebone-gunship.py -> beaglebone-minicraft.py

--- beaglebone-gunship.py
+++ beaglebone-gunship.py
@@ -4,72 +4,33 @@ Continuously read the serial port and process IO data received from a remote XBe
 
 import sys
 import time
-import Adafruit_BBIO.PWM as PWM
 import EnttecUsbDmxProWidget
+import serial
 
 dmx = EnttecUsbDmxProWidget.EnttecUsbDmxProWidget()
 dmx.setPort("/dev/ttyO2", 115200)
 dmx.connect()
+cerial = serial.Serial()
+cerial.port = "/dev/ttyO4" #Change Value once we decide.
+cerial.baudrate = 115200
+cerial.open()
+EmergencyStop = [127,127,0,127,127,0,0,0]
 
 # Continuously read and print packets
 while True:
-    rx = dmx.getRecievedFrame()
-    ResponseArray = rx['frame']
-    print(ResponseArray)
+    rx = dmx.getRecievedFrame() # we need channel
+    ResponseList = rx['frame']
+    print(ResponseList)
+    dmx.sendDMX([1,2,3,4])
     try:
-        if ResponseArray != []:            
-            if ((ResponseArray[0] == 1) and ((ResponseArray[1] < .15) and (ResponseArray[1] > -.15))): #Brake
-                PWM.set_duty_cycle("P9_14", 25.5) #Input A CHANGE ONCE JACK/RYAN GET HERE
-                PWM.set_frequency("P9_14", 10) #Frequency A
-                PWM.set_duty_cycle("P9_15", 25.5) #Input B
-                PWM.set_frequency("P9_15", 10) #Frequency B
-                sleep(.02)
-                PWM.stop("P9_14") #stops for a second
-                PWM.stop("P9_15") #stops for a second
-
-            if ((ResponseArray[0] == 1) and (ResponseArray[1] >= .25)): #Reverse
-                #PWM.set_duty_cycle("P9_14", 25.5) Input A STAY COMMENTED OUT
-                #PWM.set_frequency("P9_14", 10) STAY COMMENTED OUT
-                PWM.set_duty_cycle("P9_15", 25.5) #Input B
-                PWM.set_frequency("P9_15", 10)
-                sleep(.02)
-                PWM.stop("P9_14") #stops for a second
-                PWM.stop("P9_15") #stops for a second
-
-            if ((ResponseArray[0] == 1) and (ResponseArray[1] <= -.25)): #Forward
-                PWM.set_duty_cycle("P9_14", 25.5) #Input A 
-                PWM.set_frequency("P9_14", 10) 
-                #PWM.set_duty_cycle("P9_15", 25.5) Input B. Stay commented out.
-                #PWM.set_frequency("P9_15", 10) Stay commented out.
-                sleep(.02)
-                PWM.stop("P9_14") #stops for a second
-                PWM.stop("P9_15") #stops for a second
-            if ((ResponseArray[0] == 1) and ((ResponseArray[1] < .15) and (ResponseArray[1] > -.15))): #Brake
-                PWM.set_duty_cycle("P9_16", 25.5) #Input A CHANGE ONCE JACK/RYAN GET HERE
-                PWM.set_frequency("P9_16", 10) #Frequency A
-                PWM.set_duty_cycle("P9_17", 25.5) #Input B
-                PWM.set_frequency("P9_17", 10) #Frequency B
-                sleep(.02)
-                PWM.stop("P9_16") #stops for a second
-                PWM.stop("P9_17") #stops for a second
-
-            if ((ResponseArray[0] == 1) and (ResponseArray[1] >= .25)): #Reverse
-                #PWM.set_duty_cycle("P9_14", 25.5) Input A STAY COMMENTED OUT
-                #PWM.set_frequency("P9_14", 10) STAY COMMENTED OUT
-                PWM.set_duty_cycle("P9_17", 25.5) #Input B
-                PWM.set_frequency("P9_17", 10)
-                sleep(.02)
-                PWM.stop("P9_16") #stops for a second
-                PWM.stop("P9_17") #stops for a second
-
-            if ((ResponseArray[0] == 1) and (ResponseArray[1] <= -.25)): #Forward
-                PWM.set_duty_cycle("P9_16", 25.5) #Input A 
-                PWM.set_frequency("P9_16", 10) 
-                #PWM.set_duty_cycle("P9_15", 25.5) Input B. Stay commented out.
-                #PWM.set_frequency("P9_15", 10) Stay commented out.
-                sleep(.02)
-                PWM.stop("P9_16") #stops for a second
-                PWM.stop("P9_17") #stops for a second
+        #if gpiox == HIGH: #Change GPIO value soon.
+        if True: # Because there's no e-stop right now
+            if ResponseList != []:
+                cerial.write(bytearray([0x7E] + ResponseList + [0xE7])) #Send messages to other microcontrollers
+                       
+        else:
+            cerial.write(bytearray(EmergencyStop)) #Emergency Stop comes undone so turn off
     except KeyboardInterrupt:
         break
     time.sleep(0.01)
+cerial.close
--- beaglebone-minicraft.py
+++ beaglebone-minicraft.py
@@ -10,7 +10,7 @@ import EnttecUsbDmxProWidget
 dmx = EnttecUsbDmxProWidget.EnttecUsbDmxProWidget()
 dmx.setPort("/dev/ttyO2", 115200)
 dmx.connect()
-
+dmx.sendDMX([1,2,3,4])
 # Continuously read and print packets
 while True:
     rx = dmx.getRecievedFrame()
@@ -19,8 +19,10 @@ while True:
     try:
         if ResponseArray != []:            
             if ((ResponseArray[0] == 1) and ((ResponseArray[1] < .15) and (ResponseArray[1] > -.15))): #Brake
+                PWM.start("P9_14", 50, 1000, 1)
                 PWM.set_duty_cycle("P9_14", 25.5) #Input A CHANGE ONCE JACK/RYAN GET HERE
                 PWM.set_frequency("P9_14", 10) #Frequency A
+                PWM.start("P9_15", 50, 1000, 1)
                 PWM.set_duty_cycle("P9_15", 25.5) #Input B
                 PWM.set_frequency("P9_15", 10) #Frequency B
                 sleep(.02)
@@ -28,8 +30,7 @@ while True:
                 PWM.stop("P9_15") #stops for a second
 
             if ((ResponseArray[0] == 1) and (ResponseArray[1] >= .25)): #Reverse
-                #PWM.set_duty_cycle("P9_14", 25.5) Input A STAY COMMENTED OUT
-                #PWM.set_frequency("P9_14", 10) STAY COMMENTED OUT
+                PWM.start("P9_15", 50, 1000, 1)
                 PWM.set_duty_cycle("P9_15", 25.5) #Input B
                 PWM.set_frequency("P9_15", 10)
                 sleep(.02)
@@ -37,13 +38,38 @@ while True:
                 PWM.stop("P9_15") #stops for a second
 
             if ((ResponseArray[0] == 1) and (ResponseArray[1] <= -.25)): #Forward
+                PWM.start("P9_14", 50, 1000, 1)
                 PWM.set_duty_cycle("P9_14", 25.5) #Input A 
                 PWM.set_frequency("P9_14", 10) 
-                #PWM.set_duty_cycle("P9_15", 25.5) Input B. Stay commented out.
-                #PWM.set_frequency("P9_15", 10) Stay commented out.
                 sleep(.02)
                 PWM.stop("P9_14") #stops for a second
                 PWM.stop("P9_15") #stops for a second
+            if ((ResponseArray[0] == 1) and ((ResponseArray[1] < .15) and (ResponseArray[1] > -.15))): #Brake
+                PWM.start("P9_16", 50, 1000, 1)
+                PWM.set_duty_cycle("P9_16", 25.5) #Input A CHANGE ONCE JACK/RYAN GET HERE
+                PWM.set_frequency("P9_16", 10) #Frequency A
+                PWM.start("P9_17", 50, 1000, 1)
+                PWM.set_duty_cycle("P9_17", 25.5) #Input B
+                PWM.set_frequency("P9_17", 10) #Frequency B
+                sleep(.02)
+                PWM.stop("P9_16") #stops for a second
+                PWM.stop("P9_17") #stops for a second
+
+            if ((ResponseArray[0] == 1) and (ResponseArray[1] >= .25)): #Reverse
+                PWM.start("P9_17", 50, 1000, 1)
+                PWM.set_duty_cycle("P9_17", 25.5) #Input B
+                PWM.set_frequency("P9_17", 10)
+                sleep(.02)
+                PWM.stop("P9_16") #stops for a second
+                PWM.stop("P9_17") #stops for a second
+
+            if ((ResponseArray[0] == 1) and (ResponseArray[1] <= -.25)): #Forward
+                PWM.start("P9_16", 50, 1000, 1)
+                PWM.set_duty_cycle("P9_16", 25.5) #Input A 
+                PWM.set_frequency("P9_16", 10) 
+                sleep(.02)
+                PWM.stop("P9_16") #stops for a second
+                PWM.stop("P9_17") #stops for a second
     except KeyboardInterrupt:
         break
     time.sleep(0.01)
