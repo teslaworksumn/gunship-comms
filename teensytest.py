a5e81aa34bdcad4eac2084246a8f9a9c924a7b88
Ryan Smith <ryansmith125@gmail.com>
2015-05-04 04:30:49 -0500

    Added testing code for Teensy modules On branch master Your branch is up-to-date with 'origin/master'. Changes to be committed: 	new file:   teensytest.py

---
+++ teensytest.py
@@ -0,0 +1,60 @@
+import serial
+import sys
+import struct
+import time
+
+debug = {'SerialBuffer':False, 'RXWarning':False}
+
+if sys.version_info > (3,0):
+    py2 = False
+else:
+    py2 = True
+
+port = serial.Serial()
+port.port = sys.argv[1]
+port.baudrate = 115200
+port.open()
+
+out = 0;
+
+while True:
+    try:
+        serialbuffer = []
+        rx = b''
+        ss = "x {0} 1 2 3 4 5 6 7".format(out)
+        port.write(ss.encode())
+        print(ss)
+        out = (out+1)%256
+        print(out)
+        try:
+            if port.inWaiting() > 0:
+                rx += port.read(port.inWaiting()) # Read the buffer into a variable
+                for i in rx: # Convert the byte string into something a little more useful
+                    if py2:
+                        i =struct.unpack('B',i)[0]
+                    serialbuffer += [i]
+            rx = b''
+            si = 0
+            for i in serialbuffer: # Find the start byte
+                if i != 0x01:
+                    si += 1
+                else:
+                    break
+            if si > 0: # Remove anything before the start byte
+                if debug['RXWarning']:
+                    if debug['SerialBuffer']:
+                        print("IVD:",serialbuffer)
+                    sys.stderr.write('RX_WARNING: Removing invalid data from buffer\n')
+                serialbuffer = serialbuffer[si:-1]
+            serialbuffer = serialbuffer[0:20]
+        except:
+            e = sys.exc_info()
+            sys.stderr.write('RX_FAIL: {0}: {1}\n'.format(e[0],e[1]))
+            traceback.print_tb(e[2])
+            sys.stderr.write('Data in queue: {0}\n'.format(serialbuffer))
+        print(serialbuffer)
+        time.sleep(0.01)
+    except (KeyboardInterrupt,SystemExit):
+        break;
+
+port.close()
