969f9489c5ea963b7506788efbe2e3189aac0dc6
Ryan Smith <ryansmith125@gmail.com>
2015-05-05 01:14:58 -0500

    Added recieve code from serial, but can't relay to DS yet On branch master Your branch is up-to-date with 'origin/master'. Changes to be committed: 	modified:   beaglebone-gunship.py

--- beaglebone-gunship.py
+++ beaglebone-gunship.py
@@ -10,6 +10,8 @@ import serial
 dmx = EnttecUsbDmxProWidget.EnttecUsbDmxProWidget()
 dmx.setPort("/dev/ttyUSB0", 115200)
 dmx.connect()
+
+self.debug = {'SerialBuffer':False, 'RXWarning':False}
 cerial = serial.Serial()
 cerial.port = "/dev/ttyO4" #Change Value once we decide.
 cerial.baudrate = 115200
@@ -21,17 +23,45 @@ while True:
     rx = dmx.getRecievedFrame() # we need channel
     ResponseList = rx['frame']
     print(ResponseList)
-    dmx.sendDMX([1,2,3,4])
+    stop = False;
     try:
-        #if gpiox == HIGH: #Change GPIO value soon.
-        if True: # Because there's no e-stop right now
-            bbStr = "x {0} {1} {2} {3} {4} {5} {6} {7} {8}".format(ResponseList[0], ResponseList[1] ResponseList[2], ResponseList[3], ResponseList[4], ResponseList[5], ResponseList[6], ResponseList[7])
+        serialbuffer = []
+        rx = b''
+        if not stop: # Because there's no e-stop right now
             if ResponseList != []:
-                cerial.write(bbStr+"\r\n".encode()) #Send messages to other microcontrollers
-                print bbStr      
+                bbStr = "x {0} {1} {2} {3} {4} {5} {6} {7} {8}".format(ResponseList[0], ResponseList[1] ResponseList[2], ResponseList[3], ResponseList[4], ResponseList[5], ResponseList[6], ResponseList[7])
         else:
-            cerial.write(emergencyStop+"\r\n".encode()) #Emergency Stop comes undone so turn off
+            bbStr = emergencyStop #Emergency Stop comes undone so turn off
+        cerial.write(bbStr+"\r\n".encode()) #Send messages to other microcontrollers
+        print bbStr
+        try:
+            if port.inWaiting() > 0:
+                rx += port.read(port.inWaiting()) # Read the buffer into a variable
+                for i in rx: # Convert the byte string into something a little more useful
+                    if py2:
+                        i =struct.unpack('B',i)[0]
+                    serialbuffer += [i]
+            rx = b''
+            si = 0
+            for i in serialbuffer: # Find the start byte
+                if i != 120:
+                    si += 1
+                else:
+                    break
+            if si > 0: # Remove anything before the start byte
+                if debug['RXWarning']:
+                    if debug['SerialBuffer']:
+                        print("IVD:",serialbuffer)
+                    sys.stderr.write('RX_WARNING: Removing invalid data from buffer\n')
+                serialbuffer = serialbuffer[si:-1]
+        except:
+            e = sys.exc_info()
+            sys.stderr.write('RX_FAIL: {0}: {1}\n'.format(e[0],e[1]))
+            traceback.print_tb(e[2])
+            sys.stderr.write('Data in queue: {0}\n'.format(serialbuffer))
+        print(str(array.array('B',serialbuffer).tostring()))
+        #dmx.sendDMX()
     except KeyboardInterrupt:
         break
     time.sleep(0.01)
-cerial.close
+cerial.close()
